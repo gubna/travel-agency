426af22c981083db5db87686fd64a7bc4ba327f1
Steven Agri <stevenagri1@gmail.com>
2019-07-22 22:18:13 -0400

    changed colors of letters

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,11 +19,11 @@ const IndexPage = () => (
 
     <img src={vacation} alt="pictur" style={{maxWidth:`100%`,maxHeight:`265px`, textAlign:`center`, paddingRight:`5px`}} />
 
-    <h2 style={{color:`#14341b`, textAlign:`center`}}>Travel Agency with over 30 years experience providing expert travel advice to ensure your travel plans are just the right fit for your vacation!</h2>
+    <h2 style={{color:`#ffffff`, textAlign:`center`}}>Travel Agency with over 30 years experience providing expert travel advice to ensure your travel plans are just the right fit for your vacation!</h2>
   
     <img src={pools} alt="whatev" /><br></br>
 
-    <h2 style={{color:`#14341b`, fontSize:`1.6em`}} >We love our customers, so feel free to contact us anytime.</h2>
+    <h1 style={{color:`#ffffff`, fontSize:`1.6em`}} >We love our customers, so feel free to contact us anytime.</h1>
 
       <a title="facebook" href="https://www.facebook.com/pages/category/Travel-Agency/Joy-of-Vacations-175344059164081/">
       <img
@@ -39,8 +39,8 @@ const IndexPage = () => (
                     style={{ width: '3.6em', height: '3.6em'}}
                   />
                 </a>
-                <h3 style={{color:`#14341b`, fontSize:`1.4em` }}>joyce@joyofvacations.com</h3>
-                <h3 style={{color:`#14341b`, fontSize:`1.4em` }}>781-496-3947</h3>
+                <h3 style={{color:`#ffffff`, fontSize:`1.4em` }}>joyce@joyofvacations.com</h3>
+                <h3 style={{color:`#ffffff`, fontSize:`1.4em` }}>781-496-3947</h3>
                 </main>
 )
 
